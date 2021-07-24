974606dac4ceb98bd823805b80d3cadfa35722f4
GokulSreejith <gokulsreejith22002@gmail.com>
2021-07-24 20:07:06 +0530

    Linux config added

--- auto.js
+++ auto.js
@@ -1,24 +1,36 @@
-const {get_follow,get_repos,star,print,star_check} = require('./fun')
-const        { login, primary, secondly,wait }     = require('./async')
-const        {options,timeout,password,email }     = require('./data')
+const { get_follow, get_repos, star, star_check } = require('./fun');
+const { login, primary, secondly, wait } = require('./async');
+const { options, timeout, password, email } = require('./data_1');
 const puppeteer = require('puppeteer-core');
+const logger = require('./logger');
+const NAMESPACE = 'Auto';
+
 (async () => {
-    const browser = await puppeteer.launch(options)
-    print('Opned browser with given options','blue')
-    const page    = await browser.newPage()
-    print('Opened newtab','blue')
-    await page.setDefaultNavigationTimeout(timeout)
-    await page.goto('https://github.com/8G6?tab=followers').then(()=>{print('Gone to Followrs route','blue')})
-    print('trying to authenticate with cookies','cyan')
-    if (await page.$('button.js-profile-editable-edit-button') !== null) print('authentication sucessful !','green')
-    else{
-        print('authentication','red')
-        print('trying to login with user name and password','cyan')
-        await login(page,email,password)
-    }
-    let [follow,names] = await primary(page,get_follow) 
-    await secondly(page,follow,get_repos,names,star,star_check)
-    await browser.close()
+  const browser = await puppeteer.launch(options);
+  logger.info(NAMESPACE, 'Opned browser with given options', 'blue');
 
+  const page = await browser.newPage();
+  logger.info(NAMESPACE, 'Opened newtab in browser', 'blue');
+
+  await page.setDefaultNavigationTimeout(timeout);
+  await page.goto('https://github.com/8G6?tab=followers').then(() => {
+    logger.info(NAMESPACE, 'Gone to Followrs route', 'blue');
+  });
+  logger.info(NAMESPACE, 'trying to authenticate with cookies', 'cyan');
+
+  if ((await page.$('button.js-profile-editable-edit-button')) !== null)
+    logger.info(NAMESPACE, 'authentication sucessful !', 'green');
+  else {
+    logger.info(NAMESPACE, 'Authentication', 'red');
+    logger.info(
+      NAMESPACE,
+      'Trying to login with user name and password',
+      'cyan'
+    );
+    await login(page, email, password);
+  }
+  let [follow, names] = await primary(page, get_follow);
+  await secondly(page, follow, get_repos, names, star, star_check);
+  await browser.close();
+  logger.info(NAMESPACE, 'Browser Closed');
 })();
-
