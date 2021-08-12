5d42ab4d3a417661c7a864368d74b942c1ceda65
Devadut S Balan <56481296+8G6@users.noreply.github.com>
2021-08-12 18:05:18 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -62,32 +62,4 @@ const { info }       = require('./actions/logger');
 })();
 
 
-  logger.info(NAMESPACE, 'Opned browser with given options', 'blue');
-
-  const page = await browser.newPage();
-  logger.info(NAMESPACE, 'Opened newtab in browser', 'blue');
-
-
-  await page.setDefaultNavigationTimeout(timeout);
-  await page.goto('https://github.com/8G6?tab=followers').then(() => {
-    logger.info(NAMESPACE, 'Gone to Followrs route', 'blue');
-  });
-  logger.info(NAMESPACE, 'trying to authenticate with cookies', 'cyan');
-
-  if ((await page.$('button.js-profile-editable-edit-button')) !== null)
-    logger.info(NAMESPACE, 'authentication sucessful !', 'green');
-  else {
-    logger.info(NAMESPACE, 'Authentication', 'red');
-    logger.info(
-      NAMESPACE,
-      'Trying to login with user name and password',
-      'cyan'
-    );
-    await login(page, email, password);
-  }
-  let [follow, names] = await primary(page, get_follow);
-  await secondly(page, follow, get_repos, names, star, star_check);
-  await browser.close();
-  logger.info(NAMESPACE, 'Browser Closed');
-})();
-
+  
